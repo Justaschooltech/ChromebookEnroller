a85feed265ac1968a555a9b5040d093a3cb515ad
Ken Gardner <25060754+KenGardner@users.noreply.github.com>
2020-12-14 13:41:56 -0700

    Latest

--- ChromebookEnroller.ino
+++ ChromebookEnroller.ino
@@ -1,15 +1,6 @@
-      
-/* This is a modification of code found at http://jamesonchrome.blogspot.com/2015/08/version-44-code-with-ctrl-alt-e.html to make use of a Teensy 3.2s and Teensy-LC for automated chromebook enrollment
-   The modification handles all enrollment screens as of ChromeOS 54.*. The code does not however enter WiFi information as the expectation is
-   that the Chromebook will be attached to ethernet for enrollment. WiFi info will be supplied by policy on the Admin console. 
-   
-   Depending on your network speed or other factors you may need to tweek the delay settings. These are the settings that I found in my envirmoment allowed enough time for screens to process 
-   but not to much time you are waiting forever. You mus change the username and password in the code - lines 58 and 66
-*/
-
 #include <Bounce.h>
 
-//Pin the button is attached to as well as the Red and Green leads on the LED
+//Button and LED pins. 
 const int buttonPin = 2;
 int redPin = 9;
 int greenPin = 10;
@@ -20,6 +11,7 @@ int bluePin = 11;
 Bounce button0 = Bounce(buttonPin, 10); 
 
 void setup() {
+  
   //Give the Chromebook time to register keyboard.
   delay(2000);
     
@@ -43,7 +35,7 @@ void loop() {
   // Update the Bounce instance :
   button0.update();
 
-  //If button is not pressed set color of led to green (green means go). When pressed set color to red and peform runSteps();
+  //If button is not pressed set color of led to green (green means go). If pressed runDelay() function
   if (button0.risingEdge() ) {
     setColor(0,255,0);  
     }
@@ -66,6 +58,17 @@ void setColor(int red, int green, int blue){
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);
 }
+
+//Sets color of led to Red (red means stop), delay for 3 secons.
+void runDelay (){
+  setColor(255,0,0);
+  delay(300);
+  setColor(0,0,255);
+  delay(300);
+  setColor(255,0,0);
+  delay(300);
+  setColor(0,0,255);
+  }
 
 void runSteps(){
 //Welcome Screen******************************
@@ -111,7 +114,7 @@ Keyboard.send_now();
 delay(5000);
 
 //Username screen*********************
-Keyboard.print("YOURUSERNAME"); //types username
+Keyboard.print("chrometech@d93mail.com"); //types username
 Keyboard.set_key1(KEY_ENTER); //the next for lines tell the computer to send the Enter key
 Keyboard.send_now();
 Keyboard.set_key1(0);  //these next two lines tell the comuputer to stop sending the Enter key
@@ -119,7 +122,7 @@ Keyboard.send_now();
 delay(5000);
   
 //Password Screen*********************   
-Keyboard.print("YOURPASSWORD"); //types password
+Keyboard.print("Enr0llThem"); //types password
 Keyboard.set_key1(KEY_ENTER); //the next for lines tell the computer to send the Enter key
 Keyboard.send_now();
 Keyboard.set_key1(0);  //these next two lines tell the comuputer to stop sending the Enter key
@@ -139,8 +142,14 @@ Keyboard.send_now();
 Keyboard.set_key1(0);  //these next two lines tell the comuputer to stop sending the Enter key
 Keyboard.send_now();
 
+//return 0;
 }
 
 /*Click your mouse cursor here when uploadingotherwise the teensy will type into your code
+
+
+      YOURUSERNAME
+      YOURPASSWORD
+      
                       
 */
